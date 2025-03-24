d9058ba252153ab6f20962caaf90eb9b5d3d98ad
Tamar-Git-Hub <tamar76565@gmail.com>
2025-03-24 22:42:25 +0200

    allLost page & lostSlice

--- frontend/src/redux/slice/foundSlice.ts
+++ frontend/src/redux/slice/foundSlice.ts
@@ -1,17 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { Found } from "../../interfaces/models";
+const initialState: { allFounds: Found[] } = {
+    allFounds: [],
+};
 
-  const initialState={
-    allFounds:[]
-  }
-
-const foundSlice=createSlice({
-    name:"founds",
+const foundSlice = createSlice({
+    name: "founds",
     initialState,
-    reducers:{
-        setAllFounds:(state,action)=>{
-            state.allFounds=action.payload
+    reducers: {
+        setAllFounds: (state, action) => {
+            state.allFounds = action.payload
+        },
+        addFound: (state, action) => {
+            state.allFounds.push(action.payload)
         }
     }
 })
-export const {setAllFounds}=foundSlice.actions
+export const { setAllFounds } = foundSlice.actions
 export default foundSlice.reducer
