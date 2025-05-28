c6d09dbebc07aff1e68aee6a07c23d47eb1e4100
Tamar-Git-Hub <tamar76565@gmail.com>
2025-05-29 00:36:19 +0300

    update packake.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,22 +5,8 @@ import router from './router/router'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { CookiesProvider } from 'react-cookie'
-import { io } from "socket.io-client";
-import { useEffect } from 'react'
 
-const socket = io("http://localhost:5000"); 
 function App() {
-  useEffect(() => {
-    socket.on("newLost", (data) => {
-      alert(data.message);
-      // או להראות את ההודעה UI בצורה אחרת, לא רק alert
-      console.log("התקבלה התראה:", data);
-    });
-
-    return () => {
-      socket.off("newLost");
-    };
-  }, []);
 
 
   return (
